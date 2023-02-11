eea7d9eedeb31c71a5846eee3c04a53b3cc21b6f
Arghya Guha <96729154+Arghya003@users.noreply.github.com>
2023-02-11 22:41:21 +0530

    Added index.html

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'Demons',
+        path: 'musics/Demons - Imagine Dragons.mp3',
+        artist: 'Imagine Dragons',
+        cover: 'images/img1.jpeg'
+    },
+    {
+        name: 'Boba Tunnel',
+        path: 'musics/Boba-Tunnel-Anupam-Roy.mp3',
+        artist: 'Anupam Roy',
+        cover: 'images/img2.jpeg'
+    },
+    {
+        name: 'Jee Le  Zara',
+        path: 'musics/02 Jee Le Zaraa (Talaash).mp3',
+        artist: 'Vishal Dadlani',
+        cover: 'images/img 3.jpg'
+    },
+    {
+        name: 'Jiyen Kyun',
+        path: 'musics/38   Jiyein Kyun (Dum Maaro Dum).mp3',
+        artist: 'Papon',
+        cover: 'images/ing 4.jpeg'
+    },
+    {
+        name: 'Kicchu Chaini Ami',
+        path: 'musics/Kichchu Chaini Aami - Male (Shah Jahan Regency)- [PagalWorld.NL].mp3',
+        artist: 'Anirban Bhattacharya',
+        cover: 'images/img 5.jpg'
+    },
+]
